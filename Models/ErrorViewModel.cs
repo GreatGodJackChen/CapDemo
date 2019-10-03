c9adc9c60e83ad0d318a16064eb92e800ba1535a
Administrator <Administrator@DESKTOP-KAEJC4B>
2019-10-03 16:14:20 +0800

    添加项目文件。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CapRabbitMQDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
